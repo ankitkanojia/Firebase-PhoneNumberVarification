e01510ea64acf18b034f069b53306f810f5f4278
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-07-09 14:25:10 +0530

    Initialize react app for firebase-number auth functionality.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>App Page</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
